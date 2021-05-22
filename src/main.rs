3762541094371623249d67157c2e8b7be4b5a2a5
Ranko <ranko@fora-soft.com>
2021-05-22 20:36:31 +0300

    initial setup, code for reading a file and basic error handling

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::env;
+use std::fs;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("In file {}", config.filename);
+
+    let file_content = fs::read_to_string(config.filename).expect("Something went wrong with reading the file");
+
+    println!("With text:\n{}", file_content);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        Ok(Config { query, filename })
+    }
+}
+
